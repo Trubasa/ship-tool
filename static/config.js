f1f49a7422d407aaa9b7f3b0fde03c8f3f885bcf
trubasa <1141521502@qq.com>
2018-09-18 10:13:40 +0800

    20180918

---
+++ static/config.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Trubasa 1141521502@qq.com on 2018/5/8.
+ */
+
+
+var productConfig={
+  serverPath:'/distributionServer',  //项目名
+  apiPath:'/distributionServer/api/v1',  //接口名
+  userId:'100150816000001',
+  isDev:false,   //是否为开发模式，是的话部分连接使用固定值
+  devIp:'193.112.1.56:9000',  //开发模式下的ip,
+
+  maxResultList:100  //计算记录列表最大长度
+}
